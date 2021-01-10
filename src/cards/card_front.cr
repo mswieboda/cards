d8a6fee39e93a96e8fdb1712cfeb5f94b21d8432
Matt Swieboda <mswiebod@gmail.com>
2021-01-09 21:55:53 -0600

    draws card front to image

--- src/cards/card_front.cr
+++ src/cards/card_front.cr
@@ -1,47 +1,61 @@
 module Cards
   abstract class CardFront
+    def self.width
+      Card.width
+    end
+
+    def self.height
+      Card.height
+    end
+
+    def width
+      self.class.width
+    end
+
+    def height
+      self.class.height
+    end
+
     def draw(card : Card, screen_x, screen_y)
       draw_back(card, screen_x, screen_y)
-      draw_heading(card, screen_x, screen_y)
-      draw_rank(card, screen_x, screen_y)
     end
 
     def draw_back(card : Card, screen_x, screen_y)
       Game::RoundedRectangle.new(
         x: screen_x,
         y: screen_y,
-        width: card.width,
-        height: card.height,
+        width: width,
+        height: height,
         roundness: 0.15_f32,
         color: Game::Color::Ivory
       ).draw
     end
 
-    def draw_heading(card : Card, screen_x, screen_y)
+    def draw_heading(image : Game::Image, suit : Suit, rank : Rank)
     end
 
-    def draw_rank(card : Card, screen_x, screen_y)
-      if card.rank.numeral?
-        draw_numeral(card, screen_x, screen_y)
-      elsif card.rank.face?
-        draw_face(card, screen_x, screen_y)
-      elsif card.rank.ace?
-        draw_ace(card, screen_x, screen_y)
-      elsif card.rank.joker?
-        draw_joker(card, screen_x, screen_y)
+    def draw_rank(image : Game::Image, suit : Suit, rank : Rank)
+      if rank.numeral?
+        draw_numeral(image, suit, rank)
+      elsif rank.face?
+        draw_face(image, suit, rank)
+      elsif rank.ace?
+        draw_ace(image, suit, rank)
+      elsif rank.joker?
+        draw_joker(image, suit, rank)
       end
     end
 
-    def draw_numeral(card : Card, screen_x, screen_y)
+    def draw_numeral(image : Game::Image, suit : Suit, rank : Rank)
     end
 
-    def draw_face(card : Card, screen_x, screen_y)
+    def draw_face(image : Game::Image, suit : Suit, rank : Rank)
     end
 
-    def draw_ace(card : Card, screen_x, screen_y)
+    def draw_ace(image : Game::Image, suit : Suit, rank : Rank)
     end
 
-    def draw_joker(card : Card, screen_x, screen_y)
+    def draw_joker(image : Game::Image, suit : Suit, rank : Rank)
     end
   end
 end
--- src/cards/card_fronts/standard.cr
+++ src/cards/card_fronts/standard.cr
@@ -5,7 +5,9 @@ module Cards::CardFronts
     SPACING = 10
 
     @@sprites = {} of String => Game::Sprite
-    @@headings = {} of String => Hash(String, Game::Texture)
+    @@headings = {} of String => Hash(String, Game::Image)
+    @@images = {} of String => Hash(String, Game::Image)
+    @@textures = {} of String => Hash(String, Game::Texture)
 
     def initialize
       Suit.each do |suit|
@@ -13,7 +15,9 @@ module Cards::CardFronts
         @@sprites["ace_" + suit.to_s] = Game::Sprite.get(suit.sprite_sym).resize(32, 32)
         @@sprites["numeral_" + suit.to_s] = Game::Sprite.get(suit.sprite_sym).resize(14, 14)
 
-        @@headings[suit.to_s] = {} of String => Game::Texture
+        @@headings[suit.to_s] = {} of String => Game::Image
+        @@images[suit.to_s] = {} of String => Game::Image
+        @@textures[suit.to_s] = {} of String => Game::Texture
 
         Rank.each do |rank|
           label = Game::Image.from_text(
@@ -24,189 +28,222 @@ module Cards::CardFronts
           )
           label.flip_horizontal!
           label.flip_vertical!
-          texture_label = Game::Texture.load(label)
 
-          @@headings[suit.to_s][rank.to_s] = texture_label
+          @@headings[suit.to_s][rank.to_s] = label
+        end
+
+        Rank.each do |rank|
+          image = Game::Image.from_size(width, height)
+
+          draw_heading(image, suit, rank)
+          draw_rank(image, suit, rank)
+
+          @@images[suit.to_s][rank.to_s] = image
+          @@textures[suit.to_s][rank.to_s] = Game::Texture.load(image)
         end
       end
     end
 
-    def draw_heading(card : Card, screen_x, screen_y)
-      if card.rank.joker?
-        draw_joker_heading(card, screen_x, screen_y)
+    def draw(card : Card, screen_x, screen_y)
+      super
+
+      texture = @@textures[card.suit.to_s][card.rank.to_s]
+
+      texture.draw(x: screen_x, y: screen_y)
+    end
+
+    def draw_heading(image : Game::Image, suit : Suit, rank : Rank)
+      if rank.joker?
+        draw_joker_heading(image, suit, rank)
         return
       end
 
       # draw label
       spacing = 3
-      x = screen_x + spacing
-      y = screen_y + spacing
-      sprite = @@sprites["heading_" + card.suit.to_s]
+      x = spacing
+      y = spacing
+      sprite = @@sprites["heading_" + suit.to_s]
 
       text = Game::Text.new(
-        text: card.rank.short_name,
+        text: rank.short_name,
         size: 10,
         spacing: 2,
-        color: card.suit.color
+        color: suit.color
       )
 
       text.x = x + ((sprite.width - text.width) / 2).to_i
       text.y = y
 
-      text.draw
+      image.draw(text)
 
       # draw suit
       x += sprite.width / 2
       y += text.height + sprite.height / 2
 
-      sprite.draw(x: x, y: y, centered: true)
+      sprite.draw(image: image, x: x, y: y, centered: true)
 
       # draw label, flipped
-      label = @@headings[card.suit.to_s][card.rank.to_s]
-      x = screen_x + card.width - spacing - label.width - ((sprite.width - label.width) / 2).to_i
-      y = screen_y + card.height - spacing - label.height
+      label = @@headings[suit.to_s][rank.to_s]
+      x = width - spacing - label.width - ((sprite.width - label.width) / 2).to_i
+      y = height - spacing - label.height
 
-      label.draw(x: x, y: y)
+      image.draw(image: label, x: x, y: y)
 
       # draw suit, flipped
-      x = screen_x + card.width - spacing - sprite.width / 2
+      x = width - spacing - sprite.width / 2
       y -= sprite.height / 2
 
-      sprite.draw(x: x, y: y, rotation: 180, centered: true)
+      sprite.draw(image: image, x: x, y: y, centered: true, flip_vertical: true, flip_horizontal: true)
     end
 
-    def draw_joker_heading(card : Card, screen_x, screen_y)
+    def draw_joker_heading(image : Game::Image, suit : Suit, rank : Rank)
       spacing = 3
-      x = screen_x + spacing
-      y = screen_y + spacing
+      x = spacing
+      y = spacing
 
       %w(J O K E R).each do |label|
         text = Game::Text.new(
           text: label,
           size: 10,
           spacing: 0,
-          color: card.suit.color
+          color: suit.color
         )
 
         text.x = x
         text.y = y
 
-        text.draw
+        image.draw(text)
 
         y += text.height - 1
       end
     end
 
-    def draw_numeral(card : Card, screen_x, screen_y)
-      sprite = @@sprites["numeral_" + card.suit.to_s]
+    def draw_numeral(image : Game::Image, suit : Suit, rank : Rank)
+      sprite = @@sprites["numeral_" + suit.to_s]
       heading = 13
       spacing = 5
       spacing_v = 2
 
-      if card.rank.two? || card.rank.three?
-        x = screen_x + card.width / 2
-        y = screen_y + spacing + sprite.height
+      if rank.two? || rank.three?
+        x = width / 2
+        y = spacing + sprite.height
 
-        sprite.draw(x: x, y: y, centered: true)
+        sprite.draw(image: image, x: x, y: y, centered: true)
 
-        y = screen_y + card.height - spacing - sprite.height
+        y = height - spacing - sprite.height
 
-        sprite.draw(x: x - 1, y: y, rotation: 180, centered: true)
+        sprite.draw(image: image, x: x - 1, y: y, centered: true, flip_vertical: true, flip_horizontal: true)
       else
         spacing_h = 1
-        x = screen_x + heading + spacing_h + sprite.width / 2
-        y = screen_y + spacing + sprite.height
-        sprite.draw(x: x, y: y, centered: true)
+        x = heading + spacing_h + sprite.width / 2
+        y = spacing + sprite.height
 
-        y = screen_y + card.height - spacing - sprite.height
-        sprite.draw(x: x, y: y, rotation: 180, centered: true)
+        sprite.draw(image: image, x: x, y: y, centered: true)
 
-        x = screen_x + card.width - heading - spacing_h - sprite.width / 2
-        y = screen_y + spacing + sprite.height
-        sprite.draw(x: x, y: y, centered: true)
+        y = height - spacing - sprite.height
 
-        y = screen_y + card.height - spacing - sprite.height
-        sprite.draw(x: x, y: y, rotation: 180, centered: true)
+        sprite.draw(image: image, x: x, y: y, centered: true, flip_vertical: true, flip_horizontal: true)
+
+        x = width - heading - spacing_h - sprite.width / 2
+        y = spacing + sprite.height
+
+        sprite.draw(image: image, x: x, y: y, centered: true)
+
+        y = height - spacing - sprite.height
+
+        sprite.draw(image: image, x: x, y: y, centered: true, flip_vertical: true, flip_horizontal: true)
       end
 
-      if card.rank.three? || card.rank.five? || card.rank.nine?
-        x = screen_x + card.width / 2
-        y = screen_y + card.height / 2
+      if rank.three? || rank.five? || rank.nine?
+        x = width / 2
+        y = height / 2
 
-        sprite.draw(x: x, y: y, centered: true)
-      elsif card.rank.six? || card.rank.seven? || card.rank.eight?
+        sprite.draw(image: image, x: x, y: y, centered: true)
+      elsif rank.six? || rank.seven? || rank.eight?
         spacing_h = 1
-        x = screen_x + heading + spacing_h + sprite.width / 2
-        y = screen_y + card.height / 2
-        sprite.draw(x: x, y: y, centered: true)
+        x = heading + spacing_h + sprite.width / 2
+        y = height / 2
 
-        x = screen_x + card.width - heading - spacing_h - sprite.width / 2
-        sprite.draw(x: x, y: y, centered: true)
+        sprite.draw(image: image, x: x, y: y, centered: true)
+
+        x = width - heading - spacing_h - sprite.width / 2
+
+        sprite.draw(image: image, x: x, y: y, centered: true)
       end
 
-      if card.rank.seven? || card.rank.eight?
-        x = screen_x + card.width / 2
-        y = screen_y + card.height / 3 + spacing_v
-        sprite.draw(x: x, y: y, centered: true)
+      if rank.seven? || rank.eight?
+        x = width / 2
+        y = height / 3 + spacing_v
 
-        if card.rank.eight?
-          x = screen_x + card.width / 2
-          y = screen_y + card.height - card.height / 3 - spacing_v
-          sprite.draw(x: x - 1, y: y, rotation: 180, centered: true)
+        sprite.draw(image: image, x: x, y: y, centered: true)
+
+        if rank.eight?
+          x = width / 2
+          y = height - height / 3 - spacing_v
+
+          sprite.draw(image: image, x: x - 1, y: y, centered: true, flip_vertical: true, flip_horizontal: true)
         end
-      elsif card.rank.nine? || card.rank.ten?
+      elsif rank.nine? || rank.ten?
         spacing_h = 1
-        x = screen_x + heading + spacing_h + sprite.width / 2
-        y = screen_y + spacing + sprite.height * 2 + spacing_v
-        sprite.draw(x: x, y: y, centered: true)
+        x = heading + spacing_h + sprite.width / 2
+        y = spacing + sprite.height * 2 + spacing_v
 
-        y = screen_y + card.height - spacing - sprite.height * 2 - spacing_v
-        sprite.draw(x: x, y: y, rotation: 180, centered: true)
+        sprite.draw(image: image, x: x, y: y, centered: true)
 
-        x = screen_x + card.width - heading - spacing_h - sprite.width / 2
-        y = screen_y + spacing + sprite.height * 2 + spacing_v
-        sprite.draw(x: x, y: y, centered: true)
+        y = height - spacing - sprite.height * 2 - spacing_v
 
-        y = screen_y + card.height - spacing - sprite.height * 2 - spacing_v
-        sprite.draw(x: x, y: y, rotation: 180, centered: true)
+        sprite.draw(image: image, x: x, y: y, centered: true, flip_vertical: true, flip_horizontal: true)
+
+        x = width - heading - spacing_h - sprite.width / 2
+        y = spacing + sprite.height * 2 + spacing_v
+
+        sprite.draw(image: image, x: x, y: y, centered: true)
+
+        y = height - spacing - sprite.height * 2 - spacing_v
+
+        sprite.draw(image: image, x: x, y: y, centered: true, flip_vertical: true, flip_horizontal: true)
 
 
-        if card.rank.ten?
-          x = screen_x + card.width / 2
-          y = screen_y + card.height / 3 - spacing_v
-          sprite.draw(x: x, y: y, centered: true)
+        if rank.ten?
+          x = width / 2
+          y = height / 3 - spacing_v
 
-          x = screen_x + card.width / 2
-          y = screen_y + card.height - card.height / 3 + spacing_v
-          sprite.draw(x: x - 1, y: y, rotation: 180, centered: true)
+          sprite.draw(image: image, x: x, y: y, centered: true)
+
+          x = width / 2
+          y = height - height / 3 + spacing_v
+
+          sprite.draw(image: image, x: x - 1, y: y, centered: true, flip_vertical: true, flip_horizontal: true)
         end
       end
     end
 
-    def draw_face(card : Card, screen_x, screen_y)
+    def draw_face(image : Game::Image, suit : Suit, rank : Rank)
       text = Game::Text.new(
-        text: card.rank.name,
+        text: rank.name,
         size: 10,
         spacing: 2,
-        color: card.suit.color
+        color: suit.color
       )
 
-      text.x = (screen_x + card.width / 2 - text.width / 2).to_i
-      text.y = (screen_y + card.height / 2 - text.height / 2).to_i
+      text.x = (width / 2 - text.width / 2).to_i
+      text.y = (height / 2 - text.height / 2).to_i
 
-      text.draw
+      image.draw(text)
     end
 
-    def draw_ace(card : Card, screen_x, screen_y)
-      sprite = @@sprites["ace_" + card.suit.to_s]
+    def draw_ace(image : Game::Image, suit : Suit, rank : Rank)
+      sprite = @@sprites["ace_" + suit.to_s]
+
       sprite.draw(
-        x: screen_x + card.width / 2,
-        y: screen_y + card.height / 2,
+        image: image,
+        x: width / 2,
+        y: height / 2,
         centered: true
       )
     end
 
-    def draw_joker(card : Card, screen_x, screen_y)
+    def draw_joker(image : Game::Image, suit : Suit, rank : Rank)
     end
   end
 end
